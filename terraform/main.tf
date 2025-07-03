1c2088b4c224f94511d8ae7b077d463df5adde35
Adewale Eniola <devprecious@gmail.com>
2025-07-03 13:59:27 +0100

    Added a new s3 iam policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -265,6 +265,7 @@ resource "aws_iam_role_policy" "ec2_policy" {
         Effect = "Allow"
         Action = [
           "s3:GetObject",
+          "s3:GetObjectVersion",
           "s3:PutObject",
           "s3:DeleteObject",
           "s3:ListBucket"
